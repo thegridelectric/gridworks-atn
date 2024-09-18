3d975accf6bc93360446ded18096331ce4be3372
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-09-18 08:54:24 -0400

    get data from the winter

--- lily.py
+++ lily.py
@@ -7,18 +7,28 @@ ATN_ALIAS = "hw1.isone.ma.ng.lily"
 OUT_STUB = "/home/ubuntu/gdrive/MillinocketData/Lily"
 timezone_string = "US/Eastern"
 
+start = pendulum.datetime(2024,1,1,0,0,0,tz="America/New_York").int_timestamp
+
 maker = ScadaReportA_Maker(out_stub=OUT_STUB)
-while True:
-    print(f"{pendulum.now().in_timezone(timezone_string).strftime('%Y/%m/%d %H:%M')} Regenerating spreadsheet")
-    t = time.time()
-    time_utc = pendulum.from_timestamp(t)
-    last_utc_midnight_unix_s = t - (t % (3600 * 24))
-    last_local_midnight_unix_s = last_utc_midnight_unix_s + 3600 * (
-        time_utc.hour - time_utc.in_timezone(timezone_string).hour
-    )
+
+for day in range(120):
+    local_midnight_s = start + day*24*3600
     try:
-        maker.make_csv(last_local_midnight_unix_s, duration_hrs=24, atn_alias=ATN_ALIAS)
-    except Exception as e:
+        maker.make_csv(local_midnight_s, 24, ATN_ALIAS)
+    except Exception as e:         
         print(f"Had trouble making csv for {ATN_ALIAS}: {e}.")
 
-    time.sleep(1)
+# while True:
+#     print(f"{pendulum.now().in_timezone(timezone_string).strftime('%Y/%m/%d %H:%M')} Regenerating spreadsheet")
+#     t = time.time()
+#     time_utc = pendulum.from_timestamp(t)
+#     last_utc_midnight_unix_s = t - (t % (3600 * 24))
+#     last_local_midnight_unix_s = last_utc_midnight_unix_s + 3600 * (
+#         time_utc.hour - time_utc.in_timezone(timezone_string).hour
+#     )
+#     try:
+#         maker.make_csv(last_local_midnight_unix_s, duration_hrs=24, atn_alias=ATN_ALIAS)
+#     except Exception as e:
+#         print(f"Had trouble making csv for {ATN_ALIAS}: {e}.")
+
+#     time.sleep(1)
